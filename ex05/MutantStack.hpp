caa42b6c990497c3e85d4ac898f82502af75087c
Arthur Melin <arthur.melin@epitech.eu>
2018-01-18 20:58:13 +0100

    05

---
+++ ex05/MutantStack.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2018
+** CPP Pool Day 16
+** File description:
+** MutantStack class definition
+*/
+
+#if !defined (MUTANT_STACK_HPP_)
+	#define MUTANT_STACK_HPP_
+
+	#include <deque>
+	#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+public:
+	class iterator : public std::deque<T>::iterator {
+	public:
+		iterator(typename std::deque<T>::iterator const &it) :
+			std::deque<T>::iterator(it)
+		{
+		}
+	};
+
+	iterator begin()
+	{
+		return this->c.begin();
+	}
+
+	iterator end()
+	{
+		return this->c.end();
+	}
+};
+
+#endif /* !defined (MUTANT_STACK_HPP_) */
